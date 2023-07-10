c795356b4279471e71a800b0775396f1c211e467
yknam4222 <yyhnam2222@gmail.com>
2023-07-10 13:05:39 +0900

    123

--- Nam/Assets/PlayerController.cs
+++ Nam/Assets/PlayerController.cs
@@ -7,58 +7,25 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float walkSpeed;
 
-    [SerializeField]
-    private float lookSensitivity;
+    private float hAxis;
+    private float vAxis;
 
-    [SerializeField]
-    private float cameraRotationLimit;
-    private float currentCameraRotationX;
+    private Vector3 moveVec;
 
-    [SerializeField]
-    private Camera theCamera;
-    private Rigidbody myRigid;
+    private Animator anim;
 
-    void Start()
+    private void Awake()
     {
-        myRigid = GetComponent<Rigidbody>();  // private
+        anim = GetComponent<Animator>();
     }
 
-    void Update()  // 컴퓨터마다 다르지만 대략 1초에 60번 실행
+    private void Update()
     {
-        Move();                 // 1️⃣ 키보드 입력에 따라 이동
-        CameraRotation();       // 2️⃣ 마우스를 위아래(Y) 움직임에 따라 카메라 X 축 회전 
-        CharacterRotation();    // 3️⃣ 마우스 좌우(X) 움직임에 따라 캐릭터 Y 축 회전 
-    }
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
 
-    private void Move()
-    {
-        float _moveDirX = Input.GetAxisRaw("Horizontal");
-        float _moveDirZ = Input.GetAxisRaw("Vertical");
-        Vector3 _moveHorizontal = transform.right * _moveDirX;
-        Vector3 _moveVertical = transform.forward * _moveDirZ;
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
-
-        myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
-    }
-
-    private void CameraRotation()
-    {
-        float _xRotation = Input.GetAxisRaw("Mouse Y");
-        float _cameraRotationX = _xRotation * lookSensitivity;
-
-        currentCameraRotationX -= _cameraRotationX;
-        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
-
-        theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
-    }
-
-    private void CharacterRotation()  // 좌우 캐릭터 회전
-    {
-        float _yRotation = Input.GetAxisRaw("Mouse X");
-        Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
-        myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(_characterRotationY)); // 쿼터니언 * 쿼터니언
-        // Debug.Log(myRigid.rotation);  // 쿼터니언
-        // Debug.Log(myRigid.rotation.eulerAngles); // 벡터
+        transform.position += moveVec * walkSpeed * Time.deltaTime;
     }
 }
