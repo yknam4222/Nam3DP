56e1fee8666c5f0ee242523aa82c6a65ce7f5a1f
yknam4222 <yyhnam2222@gmail.com>
2023-08-11 13:19:52 +0900

    1

--- Nam/Assets/StatManager.cs
+++ Nam/Assets/StatManager.cs
@@ -5,9 +5,20 @@ using UnityEngine.UI;
 
 public class StatManager : MonoBehaviour
 {
+    [SerializeField]
+    private Slider HpBar;
+    [SerializeField]
+    private Slider MpBar;
+    [SerializeField]
+    private Slider StBar;
+    private void Awake()
+    {
+    }
     void Start()
     {
-
+        HpBar.GetComponent<RectTransform>().sizeDelta = new Vector2(Player.Instance.MaxHP, 50);
+        MpBar.GetComponent<RectTransform>().sizeDelta = new Vector2(Player.Instance.MaxMP, 50);
+        StBar.GetComponent<RectTransform>().sizeDelta = new Vector2(Player.Instance.MaxST, 50);
     }
 
     // Update is called once per frame
