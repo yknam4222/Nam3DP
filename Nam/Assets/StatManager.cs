8d471a2629e3e8d25152c5c480f814de42d516ef
yknam4222 <yyhnam2222@gmail.com>
2023-08-11 12:06:53 +0900

    1

---
+++ Nam/Assets/StatManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatManager : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
