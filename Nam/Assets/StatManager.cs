0ff0b2f5d28e843938556d7b0b747a4c9c13aadf
yknam4222 <yyhnam2222@gmail.com>
2023-08-14 10:28:35 +0900

    1

--- Nam/Assets/StatManager.cs
+++ Nam/Assets/StatManager.cs
@@ -5,25 +5,59 @@ using UnityEngine.UI;
 
 public class StatManager : MonoBehaviour
 {
-    [SerializeField]
-    private Slider HpBar;
-    [SerializeField]
-    private Slider MpBar;
-    [SerializeField]
-    private Slider StBar;
+    public static StatManager Instance { get { return instance; } }
+
+    private static StatManager instance;
+
+    [SerializeField] private Slider HpBar; 
+    [SerializeField] private Slider HpBar_In; 
+    [SerializeField] private Slider MpBar; 
+    [SerializeField] private Slider StBar; 
+
     private void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        else
+            DestroyImmediate(gameObject);
     }
     void Start()
+    {
+        UpdatePlayerStats();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            HpBar.value = 0.7f;
+        }
+        if(Input.GetMouseButtonDown(1))
+            StartCoroutine(UpdateHpValue());
+
+    }
+
+    public void UpdatePlayerStats()
     {
         HpBar.GetComponent<RectTransform>().sizeDelta = new Vector2(Player.Instance.MaxHP, 50);
         MpBar.GetComponent<RectTransform>().sizeDelta = new Vector2(Player.Instance.MaxMP, 50);
         StBar.GetComponent<RectTransform>().sizeDelta = new Vector2(Player.Instance.MaxST, 50);
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator UpdateHpValue()
     {
-        
+
+        while(HpBar_In.value < 0.7f)
+        {
+                HpBar_In.value -= Time.deltaTime * 5f;
+
+            yield return null;
+        }
     }
 }
